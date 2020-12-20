277874fb705c9d56901be9dd097e82f8c0c213fa
Duke Nukem <garrettduke87@gmail.com>
2020-12-20 15:40:52 -0700

    added service-worker.js

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,41 @@
+const APP_PREFIX = 'BudgetTracker-'
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    './index.html',
+    './css/styles.css',
+    './js/idb.js',
+    './js/index.js'
+ ];
+ 
+ self.addEventListener('fetch', function(event) {
+    
+    event.respondWith(caches.match(event.request).then(function (req) {
+       return req || fetch(event.req);
+    }));
+ });
+ 
+ self.addEventListener('install', function (event) {
+    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+       console.log('installing cache : ' + CACHE_NAME);
+       return cache.addAll(FILES_TO_CACHE);
+    }));
+ });
+ 
+ self.addEventListener('activate', function (e) {
+    e.waitUntil(caches.keys().then(function (keyList) {
+       let cacheKeepList = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+       });
+ 
+       cacheKeepList.push(CACHE_NAME);
+ 
+       return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+             console.log('deleting cache : ' + keyList[i]);
+             return caches.delete(keyList[i]);
+          }
+       }));
+    }));
+ });
